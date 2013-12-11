1e764b50752edac7962c7986b071addbd4ee6fe6
Hack Reactor Students <yongsoochung@gmail.com>
2013-12-10 21:27:06 -0800

    Added two more files to commit missing from last push

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,18 +11,14 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-var writeToDb = function(url, path) {
-  connection.connect();
+var writeToDb = function(url, path, callback) {
+  //connection.connect();
   connection.query('INSERT INTO urls SET ?', {url: url, path: path, timestamp: new Date()}, function(err, result) {
-    if (err) throw err;
+    if (err) { throw err; }
+    //   console.log('we ending connection now');
+    //   console.log('err: ', err);
+      callback();
   });
-
-  connection.query('SELECT * from urls', function(err, rows) {
-    if (err) throw err;
-    console.log(rows);
-  });
-
-  connection.end();
 };
 
 var getWebPage = function(url, cb) {
@@ -49,18 +45,30 @@ exports.serveStaticAssets = function(res, folder, asset) {
 };
 
 exports.writeUrlToFile = function(res, file, url) {
-  var fullPath = path.join(__dirname, "../data/sites/" + url);
-  writeToDb(url, fullPath);
-  url = url + "\n";
-  fs.appendFile(file, url, function(err) {
-    if (err) {
-      console.log(err);
-    }
 
-  headers.location = "http://127.0.0.1:8080/success.html";
+  var cb = function(){
+    console.log('we are calling callback now!!!');
+    headers.location = "http://127.0.0.1:8080/success.html";
     res.writeHead(302, headers);
+    console.log('gomma end connection now');
     res.end();
-  });
+    console.log('ok connection was ended bro');
+  };
+
+  var fullPath = path.join(__dirname, "../data/sites/" + url);
+  writeToDb(url, fullPath, cb);
+  url = url + "\n";
+  // fs.appendFile(file, url, function(err) {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+
+  // headers.location = "http://127.0.0.1:8080/success.html";
+  //   res.writeHead(302, headers);
+  //   res.end();
+  // });
+
+
   //Write some code here that helps serve up your static files!
   //(Static files are things like html (yours or arhived from others...), css, or anything that doesn't change often.)
 };
